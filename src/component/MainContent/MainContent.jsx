834ab2df75bbea009205bd45ec7ee37acf281c4e
Javeriach <javeriakanwal383@gmail.com>
2026-04-22 02:33:04 +0500

    Removed the transitions

--- src/component/MainContent/MainContent.jsx
+++ src/component/MainContent/MainContent.jsx
@@ -5,7 +5,7 @@ const MainContent = () => {
   return (
     <div className="flex flex-col lg:flex-row items-center justify-center w-full min-h-[550px] lg:h-[85vh] py-10 lg:py-0">
       <div className="w-full lg:w-[60vw] flex justify-center items-center px-6 lg:px-0 order-2 lg:order-1 mt-12 lg:mt-20">
-        <div className="w-[90vw] sm:w-[80vw] lg:w-[40vw]" data-aos="fade-right">
+        <div className="w-[90vw] sm:w-[80vw] lg:w-[40vw]">
           <h1 className="font-tektur text-center lg:text-start font-semibold text-2xl sm:text-3xl lg:text-5xl">
             Hi, I'm Javeria Kanwal
             <br />
@@ -27,14 +27,14 @@ const MainContent = () => {
         </div>
       </div>
 
-      <div className="w-full lg:w-[40vw] flex items-center justify-center order-1 lg:order-2" data-aos="fade-left">
+      <div className="w-full lg:w-[40vw] flex items-center justify-center order-1 lg:order-2">
         <div className="relative">
           <img
             src={Javeria_kanwal}
             alt="Javeria Kanwal"
-            className="rounded-full w-48 h-48 sm:w-64 sm:h-64 lg:w-80 lg:h-80 xl:w-[400px] xl:h-[400px] object-cover border-4 border-gray-100 shadow-2xl transition-all duration-300 transform"
+            className="rounded-full w-48 h-48 sm:w-64 sm:h-64 lg:w-80 lg:h-80 xl:w-[400px] xl:h-[400px] object-cover border-4 border-gray-100 shadow-2xl"
           />
-          <div className="absolute -inset-2 rounded-full bg-gradient-to-tr from-gray-200 via-transparent to-gray-300 -z-10 animate-pulse"></div>
+          <div className="absolute -inset-2 rounded-full bg-gradient-to-tr from-gray-200 via-transparent to-gray-300 -z-10"></div>
         </div>
       </div>
     </div>
